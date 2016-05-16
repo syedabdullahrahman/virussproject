4cd33ef46bc324f275e2bdd84f08d4d65802e661
Krzysiek <viruss.snk@gmail.com>
2016-05-16 12:49:50 +0200

    constructor and postconstruct methods

---
+++ viruss.plugin/src/viruss/plugin/parts/ResultsPart.java
@@ -0,0 +1,19 @@
+package viruss.plugin.parts;
+
+import viruss.plugin.helper.*;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class ResultsPart {
+
+	public ResultsPart() {
+		Helper.printClassName(this.getClass(), "Constructor");
+	}
+	
+	
+	@PostConstruct
+	public void createControls(Composite parent) {
+		Helper.printClassName(this.getClass(), "@PostConstruct");
+	}
+	
+}
