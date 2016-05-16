d0130d50bf3ac199934100e490f3a16a129a643c
Krzysiek <viruss.snk@gmail.com>
2016-05-16 10:44:54 +0200

    create Text control

--- viruss.plugin/src/viruss/plugin/parts/MainPart.java
+++ viruss.plugin/src/viruss/plugin/parts/MainPart.java
@@ -1,7 +1,12 @@
 package viruss.plugin.parts;
 
 import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.jface.layout.GridDataFactory;
 
 
 public class MainPart {
@@ -12,10 +17,26 @@ public class MainPart {
 	
 	@PostConstruct
 	public void createControls(Composite parent) {
-	  System.out.println(this.getClass().getSimpleName()
-	  + " @PostConstruct method called.");
+		// set part layout
+		final Composite comp = new Composite(parent, SWT.NONE);
+	    comp.setLayout(new GridLayout());
+	  
+	    // create Text control
+	    Text text = new Text(comp, SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL
+	        | SWT.BORDER);
+	    text.setMessage("Search");
+	    
+	    // add to layout
+	    GridDataFactory.fillDefaults().hint(130, SWT.DEFAULT).applyTo(text);
+	  
+	    System.out.println(this.getClass().getSimpleName()
+				+ " @PostConstruct method called.");
+	  
 	} 
 
+
+	 
+	
 
 	
 }
