1cec1c3532d72b8d971acb9fb66ed148ae4db6a8
Krzysiek <viruss.snk@gmail.com>
2016-05-18 16:03:11 +0200

    groups and layouts

--- viruss.plugin/src/viruss/plugin/parts/ParametersPart.java
+++ viruss.plugin/src/viruss/plugin/parts/ParametersPart.java
@@ -1,12 +1,15 @@
 package viruss.plugin.parts;
 
 import javax.annotation.PostConstruct;
-
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
+
 
 import viruss.plugin.helper.TechnicalHelper;
 
@@ -21,16 +24,29 @@ public class ParametersPart {
 		TechnicalHelper.printClassName(this.getClass(), "@PostConstruct");
 		
 		final Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new FillLayout());
 	    
-		comp.setLayout(new GridLayout(6,true));
-	    
-	    for(int i = 0; i<20; i++){
-	    	Text text = new Text(comp, SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL | SWT.BORDER);
+		Group group1 = new Group(comp, SWT.NONE);
+		group1.setLayout(new GridLayout(2,true));
+		group1.setText("parameters");
+		
+		Group group2 = new Group(comp, SWT.NONE);
+		group2.setLayout(new RowLayout());
+		group2.setText("buttons");
+		
+	    for(int i = 0; i<5; i++){
+	    	Label label = new Label(group1, SWT.BORDER);
+	    	label.setText("label " + i);
+	    	Text text = new Text(group1, SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL | SWT.BORDER);
 	    	text.setMessage("text box " + i);
-		    //GridDataFactory.fillDefaults().hint(130, SWT.DEFAULT).applyTo(text);
+		    
+	    	Button button = new Button(group2,SWT.NONE);
+	    	button.setText("Button" + i);
+	    	
+	    	//GridDataFactory.fillDefaults().hint(130, SWT.DEFAULT).applyTo(text);
+		    //GridDataFactory.fillDefaults().hint(130, SWT.DEFAULT).applyTo(label);
+		    
 	    }
-	    
-	    
 		
 	}
 	
